c50a84261a38cabb489a2fe990ca95b6c8b62217
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-11-19 16:16:45 +0100

    Corrigiendo erroes

--- client.py
+++ client.py
@@ -19,21 +19,22 @@ try:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         my_socket.connect((SERVER, port))
-        if method ==('INVITE' or 'BYE'):
-            line = ( method + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
-            print(line)
-            my_socket.send(bytes(line, 'utf-8'))
+        line = ( method + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
+        print(line)
+        my_socket.send(bytes(line, 'utf-8'))
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
+
+        if method == 'INVITE' and data.decode('utf-8').split()[-2] == '200':
+            linea = ( 'ACK' + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
+            my_socket.send(bytes(linea, 'utf-8'))
             data = my_socket.recv(1024)
-            print(data.decode('utf-8'))
-            codigo = data.decode('utf-8').split()[-2]
-            if codigo == '200':
-                line = ( 'ACK' + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
-                my_socket.send(bytes(line, 'utf-8'))
-                data = my_socket.recv(1024)
+            print (linea)
 
     print("Socket terminado.")
 
 except (IndexError, ValueError):
-	print("Usage: python3 client.py method receiver@IP:SIPport")
+    print("Usage: python3 client.py method receiver@IP:SIPport")
+
 except ConnectionRefusedError:
     print("Servidor apagado")
