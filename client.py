09ff3c89ad246adf9deafe0e4d174ad897c12d57
Marta Quintana Portales <mquintan@gamma07.aulas.gsyc.urjc.es>
2018-11-19 10:56:38 +0100

    da error en el servidor.

--- client.py
+++ client.py
@@ -3,28 +3,38 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
 SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+method= sys.argv[1]
+sip_address = sys.argv[2].split(':')[0]
+port = int(sys.argv[2].split(':')[1])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((SERVER, port))
+        if method ==('INVITE' or 'BYE'):
+            line = ( method + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
+            #if ack
+            print(line)
+            my_socket.send(bytes(line, 'utf-8'))
+            data = my_socket.recv(1024)
+            print(data.decode('utf-8'))
+            codigo = data.decode('utf-8').split()[-2]
+            if codigo == '200':
+                line = ( 'ACK' + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
+                my_socket.send(bytes(line, 'utf-8'))
+                data = my_socket.recv(1024)
+                
+    print("Socket terminado.")
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
-
-print("Fin.")
+except (IndexError, ValueError):
+	print("Usage: python3 client.py method receiver@IP:SIPport")    
+except ConnectionRefusedError:
+    print("Servidor apagado")
--- server.py
+++ server.py
@@ -3,29 +3,48 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
 import socketserver
+import os
+#os.system('comdando')
 
-
-class EchoHandler(socketserver.DatagramRequestHandler):
+class EchoHandler(socketserver.DatagramRequestHandler,cancion):
     """
     Echo server class
     """
+    
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
+            linea_decod = line.decode('utf-8').split(" ")
+            print(linea_decod)
+            if linea_decod[0] == 'INVITE':
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                
+            if linea_decod[0] == 'ACK':
+				
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + self.cancion
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
+				
+				
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        port = int(sys.argv[2])
+        serv = socketserver.UDPServer(('', port), EchoHandler)
+        print('Listening...')
+        try:
+            serv.serve_forever()
+        except KeyboardInterrupt:
+            print("Finalizado servidor")
+    except (IndexError, ValueError, PermissionError):
+        print("Usage: phython3 server.py IP port audio_file")
