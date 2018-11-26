a30c238e570ebd997fe39862a29913aebc745983
Marta Quintana Portales <mquintan@gamma07.aulas.gsyc.urjc.es>
2018-11-26 09:19:20 +0100

    Corregido Error de ACK.

--- client.py
+++ client.py
@@ -19,22 +19,24 @@ try:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         my_socket.connect((SERVER, port))
-        line = (method + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
-        print(line)
-        my_socket.send(bytes(line, 'utf-8'))
-        data = my_socket.recv(1024)
-        print(data.decode('utf-8'))
+        if method == 'INVITE' or method == 'BYE':
+            line = (method + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
+            print(line)
+            my_socket.send(bytes(line, 'utf-8'))
+            data = my_socket.recv(1024)
+            print(data.decode('utf-8'))
+        else:
+            print("Solo puedes enviar Métodos INVITE o BYE")
 
         if method == 'INVITE' and data.decode('utf-8').split()[-2] == '200':
             linea = ('ACK' + ' sip:' + sip_address + ' SIP/2.0\r\n\r\n')
             my_socket.send(bytes(linea, 'utf-8'))
             data = my_socket.recv(1024)
             print(data.decode('utf-8'))
-
     print("Socket terminado.")
 
 except (IndexError, ValueError):
     print("Usage: python3 client.py method receiver@IP:SIPport")
 
 except ConnectionRefusedError:
-    print("Servidor apagado")
+    print("Servidor apagado / Error de puerto. ")
